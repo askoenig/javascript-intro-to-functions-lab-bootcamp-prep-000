49966425aa513a42290607704ead4f1ccc31b864
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-13 18:58:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ function logShout(string) {console.log(`HELLO`)
 function logWhisper(string) {console.log(`hello`)
 }
 
-function sayHiToGrandma(string) {
-  if(string === whisper(string)) {return ("I can't hear you!")
- 
-  } else if(string === shout(string)) {return ("YES INDEED!")
-
-  }
-  
-  else if(string === "I love you, Grandma.") {return ("I love you, too.")
-  
-} }
 
 function sayHiToGrandma(string) {
   if (string === whisper(string)) {
